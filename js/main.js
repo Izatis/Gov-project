b5019c82415a46e50cdd3987a949d3d37b1fc3aa
Izatis <mekinovizat1@gmail.com>
2023-05-04 23:08:22 +0600

    add: card

--- js/main.js
+++ js/main.js
@@ -1,65 +1,70 @@
 // Карточки свайпера
 const slides = [
   {
-    image: './images/random.jpg',
-    description: 'До 2026 года 300 тысяч высокооплачиваемых рабочих мест в Кыргызстане  планирует создать кабмин'
+    image: "./images/random.jpg",
+    description:
+      "До 2026 года 300 тысяч высокооплачиваемых рабочих мест в Кыргызстане  планирует создать кабмин",
   },
   {
-    image: './images/42227f99ce45d942a1824c6beadbb4e4.jpg',
-    description: 'До 2026 года 300 тысяч высокооплачиваемых рабочих мест в Кыргызстане  планирует создать кабмин'
+    image: "./images/42227f99ce45d942a1824c6beadbb4e4.jpg",
+    description:
+      "До 2026 года 300 тысяч высокооплачиваемых рабочих мест в Кыргызстане  планирует создать кабмин",
   },
   {
-    image: './images/772604.c2d590a5b031d53cc9add60339607368.jpg',
-    description: 'До 2026 года 300 тысяч высокооплачиваемых рабочих мест в Кыргызстане  планирует создать кабмин'
+    image: "./images/772604.c2d590a5b031d53cc9add60339607368.jpg",
+    description:
+      "До 2026 года 300 тысяч высокооплачиваемых рабочих мест в Кыргызстане  планирует создать кабмин",
   },
   {
-    image: './images/Kyrgyzatan_Summary_tcm10-33237.jpg',
-    description: 'До 2026 года 300 тысяч высокооплачиваемых рабочих мест в Кыргызстане  планирует создать кабмин'
-  }
+    image: "./images/Kyrgyzatan_Summary_tcm10-33237.jpg",
+    description:
+      "До 2026 года 300 тысяч высокооплачиваемых рабочих мест в Кыргызстане  планирует создать кабмин",
+  },
 ];
 
-const slidesHtml = slides.map((slide) => `
+const slidesHtml = slides
+  .map(
+    (slide) => `
   <div class="slide swiper-slide">
-    <div class="slide__image">
-      <img src="${slide.image}" alt="random" class="image my-image" />
+    <div class="slide__image my-image">
+      <img src="${slide.image}" alt="random"/>
     </div>
     <div class="slide__info">
       <span class="text container-text">${slide.description}</span>
       <span class="date"></span>
     </div>
   </div>
-`).join('');
+`
+  )
+  .join("");
 
-document.querySelector('.swiper-wrapper').innerHTML = slidesHtml;
-
+document.querySelector(".swiper-wrapper").innerHTML = slidesHtml;
 
 // Устанавливаем обновляемую дату
 function updateDate() {
-    var now = new Date();
-    var dateElements = document.querySelectorAll('.date');
-    var options = { year: 'numeric', month: 'long', day: 'numeric' };
-    dateElements.forEach(function (dateElement) {
-      dateElement.innerHTML = now.toLocaleDateString('ru-RU', options);
-    });
+  var now = new Date();
+  var dateElements = document.querySelectorAll(".date");
+  var options = { year: "numeric", month: "long", day: "numeric" };
+  dateElements.forEach(function (dateElement) {
+    dateElement.innerHTML = now.toLocaleDateString("ru-RU", options);
+  });
 }
 
 setInterval(updateDate, 100);
 
-
 // Открывает версию для слабовидящих
-const icon = document.querySelector('.eye-burger i');
-const link = document.querySelector('.eye-burger .eye__title');
-const accessibilityHeader = document.querySelector('.accessibility-header');
+const icon = document.querySelector(".eye-burger i");
+const link = document.querySelector(".eye-burger .eye__title");
+const accessibilityHeader = document.querySelector(".accessibility-header");
 
-const toggleAccessibilityHeader = function() {
-  accessibilityHeader.classList.toggle('show-accessibility-header');
+const toggleAccessibilityHeader = function () {
+  accessibilityHeader.classList.toggle("show-accessibility-header");
 };
 
-icon.addEventListener('click', toggleAccessibilityHeader);
-link.addEventListener('click', toggleAccessibilityHeader);
+icon.addEventListener("click", toggleAccessibilityHeader);
+link.addEventListener("click", toggleAccessibilityHeader);
 
-
-  // Изменение кнопок
+// Изменение кнопок
 const fontSizeLargeBtn = document.getElementById("font-size-large");
 const fontSizeMediumBtn = document.getElementById("font-size-medium");
 const fontSizeSmallBtn = document.getElementById("font-size-small");
@@ -95,8 +100,6 @@ letterSpacingSmallBtn.addEventListener("click", () => {
   document.body.style.letterSpacing = "0.05em";
 });
 
-
-
 // // Обработчик клика на кнопке Изображения
 const bwButtons = document.querySelectorAll(".bw-button");
 const colorButtons = document.querySelectorAll(".color-button");
@@ -119,7 +122,6 @@ colorButtons.forEach((button) => {
     });
   });
 });
-
 
 // Cбрасывает все стили
 const resetStylesBtn = document.getElementById("reset-styles-button");
